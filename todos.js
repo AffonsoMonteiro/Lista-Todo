37ef3005cd8df32c9b9de3c880612d47824fb469
AffonsoMonteiro <afonsomonteiro_@hotmail.com>
2020-07-25 13:22:54 -0300

    Update todos.js

--- todos.js
+++ todos.js
@@ -1,12 +1,14 @@
 var listElement = document.querySelector('#app ul');
 var inputElement = document.querySelector('#app input');
 var buttonElement = document.querySelector('#app button');
-
+/*
 var todos = [
     'Fazer Café',
     'Estudar JavaScript',
     'Ultilizar o GitHub'
 ];
+*/
+    var todos =JSON.parse(localStorage.getItem('list_todos')) || [];
 
 function renderTodos() {
     listElement.innerHTML = '';
@@ -40,6 +42,7 @@ function addTodo() {
     todos.push(todoText);
     inputElement.value = '';
     renderTodos();
+    saveToStorage();
 }
 
 buttonElement.onclick = addTodo;
@@ -47,9 +50,10 @@ buttonElement.onclick = addTodo;
 function deleteTodo(pos) {
     todos.splice(pos, 1);
     renderTodos();
+    saveToStorage();
 }
 
 
 function saveToStorage() {
-    localStorage.setItem('list_todos')
+    localStorage.setItem('list_todos', JSON.stringify(todos));
 }
