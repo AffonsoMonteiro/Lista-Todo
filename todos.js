8853ef85e286fdc7089d87b361462649cc1e15bb
AffonsoMonteiro <afonsomonteiro_@hotmail.com>
2020-07-25 12:18:43 -0300

    Update todos.js

--- todos.js
+++ todos.js
@@ -8,3 +8,14 @@ var todos = [
     'Ultilizar o GitHub'
 ];
 
+function renderTodos() {
+    for (todo of todos) {
+        var todoElement = document.createElement('li');
+        var todoText = document.createTextNode(todo)
+
+        todoElement.appendChild(todoText)
+        listElement.appendChild(todoElement)
+    }
+}
+
+renderTodos()
